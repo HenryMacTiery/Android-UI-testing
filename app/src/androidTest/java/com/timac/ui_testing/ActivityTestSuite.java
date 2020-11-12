4449133d41c6208c62599e897383a1ea6816db7f
HenryMacTiery <machariawaribu@gmail.com>
2020-11-12 09:32:47 +0300

    Utilized a Test Suite to perform all tests by combining them into 1 test file

---
+++ app/src/androidTest/java/com/timac/ui_testing/ActivityTestSuite.java
@@ -0,0 +1,13 @@
+package com.timac.ui_testing;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses(
+        {MainActivityTest.class,
+        SecondaryActivityTest.class}
+)
+public class ActivityTestSuite {
+
+}

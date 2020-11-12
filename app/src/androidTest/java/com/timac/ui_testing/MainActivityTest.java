89e95106e40906ce85c7b128f02e73f6e6d20089
HenryMacTiery <machariawaribu@gmail.com>
2020-11-12 09:13:05 +0300

    Activity Navigation Testing

--- app/src/androidTest/java/com/timac/ui_testing/MainActivityTest.java
+++ app/src/androidTest/java/com/timac/ui_testing/MainActivityTest.java
@@ -1,7 +1,6 @@
 package com.timac.ui_testing;
 
 
-import androidx.test.espresso.matcher.ViewMatchers;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner;
 
@@ -10,11 +9,11 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.Espresso.pressBack;
+import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static androidx.test.espresso.matcher.ViewMatchers.withEffectiveVisibility;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
-import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
 @RunWith(AndroidJUnit4ClassRunner.class)
 public class MainActivityTest {
@@ -24,28 +23,31 @@ public class MainActivityTest {
             new ActivityScenarioRule<>(MainActivity.class);
 
     @Test
-    public void test_isActivityInView() {
-        onView(withId(R.id.main)).check(matches(isDisplayed()));
-    }
-
-    @Test
-    public void test_visibility_title_nextButton() {
-        onView(withId(R.id.activity_main_title))
+    public void test_navSecondaryActivity() {
+        onView(withId(R.id.btn_next_activity))
+                .perform(click());
+        onView(withId(R.id.secondary))
                 .check(matches(isDisplayed()));
-
-        onView(withId(R.id.btn_next_activity))
-                .check(matches(isDisplayed()));     // Method 1 to test Visibility
     }
 
     @Test
-    public void button_nextActivity() {
+    public void test_backPress_toMainActivity() {
         onView(withId(R.id.btn_next_activity))
-                .check(matches(withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE))); // Method 2 to test Visibility
+                .perform(click());
+        onView(withId(R.id.secondary))
+                .check(matches(isDisplayed()));
+        onView(withId(R.id.btn_back_activity))
+                .perform(click());      //Method 1
+        onView(withId(R.id.main))
+                .check(matches(isDisplayed()));
     }
 
     @Test
-    public void test_isTitleTextDisplayed() {
-        onView(withId(R.id.activity_main_title))
-                .check(matches(withText(R.string.text_mainactivity)));
+    public void test_backPress_toMainActivity_method2() {
+        onView(withId(R.id.btn_next_activity))
+                .perform(click());
+        pressBack();
+        onView(withId(R.id.main))
+                .check(matches(isDisplayed()));
     }
 }
